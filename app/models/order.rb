26a55f44939f2e0876e5de54e189f4828a089ac6
NU-MORIYA <animopara100pre@gmail.com>
2020-10-18 08:57:47 +0000

    [modify] controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ApplicationRecord
   has_many :ordered_items, dependent: :destroy
-  has_many :orderedes, through: :ordered_items, source: :item
+  has_many :items, through: :ordered_items
   belongs_to :member
   enum payment_method: { クレジットカード: 0, 銀行振り込み: 1 }
   enum order_status: {入金待ち: 0, 入金確認中: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
