4a00743ed6b26c1a2ebc31afc47fde146064b127
EC2 Default User <ec2-user@ip-172-31-36-67.ec2.internal>
2020-10-19 06:26:37 +0000

    first commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_16_052312) do
+ActiveRecord::Schema.define(version: 2020_10_17_071031) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -74,6 +74,7 @@ ActiveRecord::Schema.define(version: 2020_10_16_052312) do
     t.boolean "is_deleted", default: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "phone_number"
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
   end
