547a388a5b04d1ba5f63b779b5652701abad03f8
EC2 Default User <ec2-user@ip-172-31-63-19.ec2.internal>
2020-10-20 15:20:20 +0000

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,43 +1,4 @@
 Rails.application.routes.draw do
-
-  # namespace :admin do
-  #   get 'members/genres'
-  #   get 'members/items'
-  #   get 'members/orders'
-  #   get 'members/ordered_items'
-  #   get 'members/homes'
-  # end
-
-  # devise_for :admins
-  # devise_for :members
-
-  # namespace :admin do
-  #   resources :members,:genres, except:[:show, :new, :destroy]
-  #   resources :items, except:[:destroy]
-  #   resources :orders, except:[:destroy, :edit]
-  #   resources :ordered_items, only:[:update]
-  #   root 'homes#top'
-  # end
-
-  # namespace :public do
-  #   resource :member, except:[:create, :new, :destroy]
-  #   resources :delivery_destinations, except:[:show, :new]
-  #   resources :items, except:[:destroy]
-  #   resources :cart_items, except:[:show, :new, :edit]
-  #   resources :orders, except:[:edit, :update, :destroy]
-  #   root 'home#top'
-  #   get 'homes/about' => 'homes#about'
-  #   get 'orders/thanks' => 'orders#thanks'
-  #   get 'orders/check' => 'orders#check'
-  #   get 'members/leave' => 'members#leave'
-  #   patch 'members/actived' => 'members#actived'
-  #   put 'members/actived' => 'members#actived'
-  # end
-
-  # scope module: :public do
-  #       resource :member, except:[:create, :new, :destroy]
-  #       delete 'cart_items/all_destroy' => 'cart_items#all_destroy'
-  # end
   devise_for :admins
   
   namespace :admin do
