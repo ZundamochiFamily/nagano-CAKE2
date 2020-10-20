649b834d1a50f71e997df7cba89ad958dd24e6e3
EC2 Default User <ec2-user@ip-172-31-63-19.ec2.internal>
2020-10-20 06:11:58 +0000

    コントローラーitemモデルのカラム変更

--- app/controllers/public/members_controller.rb
+++ app/controllers/public/members_controller.rb
@@ -1,2 +1,2 @@
-class MembersController < ApplicationController
+class Public::MembersController < ApplicationController
 end
--- db/migrate/20201016024257_create_items.rb
+++ db/migrate/20201016024257_create_items.rb
@@ -6,7 +6,7 @@ class CreateItems < ActiveRecord::Migration[5.2]
       t.text :introduction
       t.integer :tax_excluded_price
       t.string :item_image_id
-      t.string :sales_status, default: 0
+      t. :sales_status, default: 0
       t.timestamps
     end
   end
