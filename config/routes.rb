fe5e19c6ba954a4285f894fdbb7c03e1a9892de3
EC2 Default User <ec2-user@ip-172-31-63-19.ec2.internal>
2020-10-20 05:57:50 +0000

    ルーティング変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,37 +1,42 @@
 Rails.application.routes.draw do
 
-  namespace :admin do
-    get 'members/genres'
-    get 'members/items'
-    get 'members/orders'
-    get 'members/ordered_items'
-    get 'members/homes'
-  end
   devise_for :admins
-  devise_for :members
-
   namespace :admin do
-    resources :members,:genres, except:[:show, :new, :destroy]
+    resources :members,except:[:new, :create, :destroy]
+    resources :genres, except:[:new, :show, :destroy]
     resources :items, except:[:destroy]
-    resources :orders, except:[:destroy, :edit]
+    resources :orders, except:[:new, :create, :edit, :destroy]
     resources :ordered_items, only:[:update]
     root 'homes#top'
   end
-
-  namespace :public do
-    resource :member, except:[:create, :new, :destroy]
+  devise_for :members, :members => {
+    :sessions => 'members/sessions',
+    :registrations => 'members/registrations',
+    :passwords => 'members/passwords'
+  }
+  scope module: :public do
+    root 'homes#top'
+    get 'homes/about' => 'homes#about'
+    resource :member, except:[:create, :new, :destroy] do
+      collection do
+        get 'leave'
+        put 'actived'
+        patch 'actived'
+      end 
+    end
     resources :delivery_destinations, except:[:show, :new]
     resources :items, except:[:destroy]
-    resources :cart_items, except:[:show, :new, :edit]
-    resources :orders, except:[:edit, :update, :destroy]
-    root 'home#top'
-    get 'homes/about' => 'homes#about'
-    get 'orders/thanks' => 'orders#thanks'
-    get 'orders/check' => 'orders#check'
-    get 'members/leave' => 'members#leave'
-    patch 'members/actived' => 'members#actived'
-    put 'members/actived' => 'members#actived'
+    resources :cart_items, except:[:show, :new, :edit] do
+      collection do
+        delete '/' => 'cart_items#all_destroy'
+      end
+    end
+    resources :orders, except:[:edit, :update, :destroy] do
+      collection do
+        get 'check'
+        get 'thanks'
+      end
+    end
   end
-
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
