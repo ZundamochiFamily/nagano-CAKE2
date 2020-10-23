b5510b2782b017bdcf987241f6fdedf9f7f0373e
EC2 Default User <ec2-user@ip-172-31-63-19.ec2.internal>
2020-10-23 05:37:12 +0000

    updata

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,5 +4,9 @@ class Order < ApplicationRecord
   belongs_to :member
   enum payment_method: { クレジットカード: 0, 銀行振り込み: 1 }
   enum order_status: {入金待ち: 0, 入金確認中: 1, 製作中: 2, 発送準備中: 3, 発送済み: 4 }
-end
 
+  def purchased_price
+    purchased_price = order_detail.item.tax_excluded_price*order_detail.quantity
+  end
+
+end
