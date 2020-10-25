491889894322e0e89cca526e3c22d5e472f6b66d
EC2 Default User <ec2-user@ip-172-31-79-9.ec2.internal>
2020-10-25 04:34:52 +0000

    up

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,14 +2,7 @@ class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :orderd_items, dependent: :destroy
   belongs_to :genre
-<<<<<<< HEAD
-
- #enum sales_status:{onsale:0,soldout:1}
 
  attachment :item_image
 end
 
-=======
-  attachment :item_image
-end
->>>>>>> 647bda17bc7dacb6759bd9a2388472189bb21479
