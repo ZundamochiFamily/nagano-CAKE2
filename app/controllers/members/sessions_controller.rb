fa7707f8051ba0258e8da1fa5df4a7fb21509895
EC2 Default User <ec2-user@ip-172-31-36-67.ec2.internal>
2020-10-25 07:55:52 +0000

    modify leave

--- app/controllers/members/sessions_controller.rb
+++ app/controllers/members/sessions_controller.rb
@@ -3,6 +3,23 @@
 class Members::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
 
+  before_action :reject_member, only: [:create]
+
+  protected
+
+  def reject_member
+    @member = Member.find_by(email: params[:member][:email].downcase)
+    if @member
+      if (@member.valid_password?(params[:member][:password]) && (@member.active_for_authentication? == false))
+        flash[:error] = "このユーザーは退会済みです。"
+        redirect_to new_member_session_path
+      end
+    else
+      flash[:error] = "必須項目を入力してください。"
+    end
+  end
+
+
   # GET /resource/sign_in
   # def new
   #   super
