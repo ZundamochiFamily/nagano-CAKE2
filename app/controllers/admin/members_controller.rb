73af9b7a788ab9a6ecfc1eccca073e05afea1c5c
EC2 Default User <ec2-user@ip-172-31-79-9.ec2.internal>
2020-10-26 07:57:10 +0000

    up

--- app/controllers/admin/members_controller.rb
+++ app/controllers/admin/members_controller.rb
@@ -1,6 +1,4 @@
 class Admin::MembersController < ApplicationController
-<<<<<<< HEAD
-=======
 
   def index
     @members = Member.all
@@ -10,9 +8,12 @@ class Admin::MembersController < ApplicationController
     @member = Member.find(params[:id])
   end
 
+
+
   def edit
     @member = Member.find(params[:id])
   end
 
->>>>>>> 18066bc08245cc68b20f5e7aa468eaac8eaddad6
+
+
 end
