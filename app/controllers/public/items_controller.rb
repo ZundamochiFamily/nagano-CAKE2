3a8c9db2b87a4018b1ac613659974ec687a7ebfd
NU-MORIYA <animopara100pre@gmail.com>
2020-10-27 01:47:55 +0000

    [modify]public/view.controller_items_tax

--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -3,12 +3,14 @@ class Public::ItemsController < ApplicationController
   def index
     @items = Item.all
     @genres = Genre.all.where(is_activated: 'true')
+    @tax = 1.1
   end
 
   def show
     @genres = Genre.all.where(is_activated: 'true')
     @item = Item.find(params[:id])
     @cart_item = CartItem.new
+    @tax = 1.1
   end
 
 end
